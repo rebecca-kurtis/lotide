9591ba8f87f7420d95ea43f2a9b20fb4ebf91597
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-10-19 14:46:16 -0700

    creating our own assertion code to test if the code is correct or not

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(10, 10);
+assertEqual("hi", "Hi");
+assertEqual("love", "love");
+assertEqual(1, 4);
