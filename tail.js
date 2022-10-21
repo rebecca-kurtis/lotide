cd9b0fe519b27b14478fd8ccf1c5c7aefd19adb1
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-10-21 16:47:54 -0700

    adding tails.js after linting code

--- tail.js
+++ tail.js
@@ -7,25 +7,25 @@ const assertEqual = function(actual, expected) {
 
 };
 
-//needs to return the tail of an array - everything except the first item 
-// it should not modify the array, just return a new array. 
-// an array with only one element should yield an empty array for its tail 
-// an empty array should yield an empty array for its tail. 
+//needs to return the tail of an array - everything except the first item
+// it should not modify the array, just return a new array.
+// an array with only one element should yield an empty array for its tail
+// an empty array should yield an empty array for its tail.
 
-const tail = function (data) {
+const tail = function(data) {
 //console.log(newArray);
-if (data.length === 1) {
-  return [];
-} 
-let newArray = data.slice([1])
-return newArray; 
-}
+  if (data.length === 1) {
+    return [];
+  }
+  let newArray = data.slice([1]);
+  return newArray;
+};
 
 
 
 
 
-// Test Case: Check the original array 
+// Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
