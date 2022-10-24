93524fb533f83ca29d0206e22fbe0175f5f7405e
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-10-24 14:26:10 -0700

    linted current code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -7,17 +7,18 @@
 
 }; */
 
-const assertArraysEqual = function (array1, array2) {
-  for (let x = 0; x < array1.length; x++ ) {
-      if (array1[x] !== array2[x]) {
-        return console.log(`❌❌❌ ${array1} is not equal to ${array2}`);}
-  } 
+const assertArraysEqual = function(array1, array2) {
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return console.log(`❌❌❌ ${array1} is not equal to ${array2}`);
+    }
+  }
   return console.log(`✅✅✅ ${array1} is equal to ${array2}`);
-}; 
+};
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
 
-eqArrays([1, 2, 3], [3, 2, 1]); // => should PASS
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should PASS
 
