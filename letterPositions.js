9277c9f84af6c0c19890e74358188b9928f27ff9
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-10-26 14:04:52 -0700

    adding letterPositions assignment to project folder

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const eqArrays = function(array1, array2) {
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+
+  for (let x = 0; x < array1.length; x++) {
+    if (array1[x] !== array2[x]) {
+      console.log(`❌❌❌ ${array1} is not equal to ${array2}`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ ${array1} is equal to ${array2}`);
+  return;
+};
+
+//function to build
+
+const letterPositions = function(sentence) {
+  const results = {};
+  //logic to update results here
+  //needs to return an array of the indicies
+
+  for (let x = 0; x < sentence.length; x++) {
+    if (sentence[x] !== " ") {
+
+      if (results[sentence[x]]) {
+        results[sentence[x]].push(x);
+      } else {
+        results[sentence[x]] = [x];
+      }
+    }
+  }
+  console.log(results);
+  return results;
+};
+
+
+// Test code
+
+letterPositions("hello hi");
