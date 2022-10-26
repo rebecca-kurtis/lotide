01d3a9a80e71fb1456983260836084ee6d1343f4
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-10-26 11:20:26 -0700

    Adding eslint changes

--- countOnly.js
+++ countOnly.js
@@ -1,9 +1,9 @@
 //this function should take in a collection of items and return counts for a specific subset of those items.
 //it won't count everything. In order to decide what to count it will also be given an idea of the items we care about and it will only count those ignoring the others.
-//items in our case will be limited to strings. 
+//items in our case will be limited to strings.
 //countOnly will be given an array and an object - it will return an object containing counts of everything that the input object listed.
 
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     return;
@@ -18,8 +18,8 @@ const assertEqual = function (actual, expected) {
 //itemsToCount: an object specifying what to count
 // Functino should report back how many instances of each string were found in allItems
 
-const countOnly = function (allItems, itemsToCount) {
-  const results = {}
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
 
   for (const item of allItems) {
     if (itemsToCount[item]) {
