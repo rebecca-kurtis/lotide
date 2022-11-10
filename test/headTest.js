6dd2b46b7cc5e60255ed12eac1466130bbe66946
Rebecca Kurtis <rebecca.kurtis@hotmail.com>
2022-11-09 16:56:55 -0800

    added mocha/chai testing and refactored HeadTest files

--- test/headTest.js
+++ test/headTest.js
@@ -1,11 +1,27 @@
+const assert = require('chai').assert;
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
-console.log(head([1, 2, 3, 4, 5]));
-console.log(head([]));
-console.log(head(["a", "b", "c", "d"]));
+describe("#head", () => {
 
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]),);
+  it("returns 5 for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns [] for []", () => {
+    assert.strictEqual(head([]),);
+  });
+
+});
